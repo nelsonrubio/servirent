e977d0ef2d5ed1bad181e7c1496ed1fd892356c1
Nelson Rubio <nrubio@cobeca.com>
2021-05-07 14:23:28 -0400

    Cambios en la base de datos

--- conexion/servirent.sql
+++ conexion/servirent.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-04-2021 a las 17:08:00
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.5
+-- Tiempo de generación: 07-05-2021 a las 20:22:30
+-- Versión del servidor: 10.4.16-MariaDB
+-- Versión de PHP: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,19 +42,17 @@ CREATE TABLE `articulos` (
   `Proveedor` varchar(500) NOT NULL,
   `CodigoEquipo` varchar(500) NOT NULL,
   `nota` varchar(2000) NOT NULL,
-  `directorioImagen` varchar(500) NOT NULL
+  `directorioImagen` varchar(500) NOT NULL,
+  `idBodega` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `articulos`
 --
 
-INSERT INTO `articulos` (`idArticulo`, `nombreHerramienta`, `marca`, `modelo`, `nroSerie`, `PrecioDia`, `PrecioHora`, `PrecioSemana`, `PrecioMes`, `fechaCompra`, `NroFactura`, `Proveedor`, `CodigoEquipo`, `nota`, `directorioImagen`) VALUES
-(1, 'nombre', '', 'modelo', 1, '2', '3', '4', '5', '0000-00-00', 6, '', '7', '0', ''),
-(2, 'Martillo', 'stanly', 'modelo de prueba', 2323, '500', '2323', '2323', '23232', '0000-00-00', 343554641, 'martillos orientee', '40011', 'hola', ''),
-(3, 'Taladro de pared', 'mankitar', 'KN67-23R', 123456801, '10000', '500', '50000', '100000', '0000-00-00', 20304050, 'Daka', '1012', 'Este talador es para el acero.', ''),
-(4, 'dfdfdf', 't', 'dfdfdf', 4343434, '343434', '3443434', '43434343', '5454545', '0000-00-00', 34343434, 'cvcvcvcv', '34343434', 'fdsfdsfds', 'articulos/IMG_20200427_125451_203.jpg'),
-(5, 'Pala', 'stanly', 'KN67-23R', 123456789, '30', '10', '150', '300', '2021-04-02', 123456789, 'palita', '987654321', 'Herramienta nueva', 'articulos/Koala.jpg');
+INSERT INTO `articulos` (`idArticulo`, `nombreHerramienta`, `marca`, `modelo`, `nroSerie`, `PrecioDia`, `PrecioHora`, `PrecioSemana`, `PrecioMes`, `fechaCompra`, `NroFactura`, `Proveedor`, `CodigoEquipo`, `nota`, `directorioImagen`, `idBodega`, `cantidad`) VALUES
+(30, 'Martillo 1', 'stanly', '343j4kjfr', 1212122, '20', '30', '50', '100', '0000-00-00', 125454, 'nelson', 'jhfjdhfu34', 'Herramienta nueva', '', 1, 165);
 
 -- --------------------------------------------------------
 
@@ -74,10 +72,11 @@ CREATE TABLE `bodegas` (
 --
 
 INSERT INTO `bodegas` (`idBodega`, `nombreBodega`, `fechaCreacion`, `tipoBodega`) VALUES
-(1, '', '2021-04-14', 1),
-(2, '', '2021-04-14', 1),
-(3, 'Bodega de prueba', '2021-04-14', 1),
-(4, 'Bodega de prueba 2', '2021-04-14', 2);
+(1, 'Bodega de prueba ', '2021-04-14', 1),
+(2, 'Bodega de prueba 1', '2021-04-14', 1),
+(3, 'Bodega de prueba 2', '2021-04-14', 1),
+(4, 'Bodega de prueba 3', '2021-04-14', 2),
+(5, 'Bodega respuesto', '2021-05-04', 2);
 
 -- --------------------------------------------------------
 
@@ -95,17 +94,21 @@ CREATE TABLE `cabeceranota` (
   `fechaFin` date NOT NULL,
   `nombreObra` varchar(500) NOT NULL,
   `responsableObra` varchar(500) NOT NULL,
-  `estatusNota` int(11) NOT NULL
+  `estatusNota` int(11) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `idChofer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `cabeceranota`
 --
 
-INSERT INTO `cabeceranota` (`idcabeceranota`, `nombreAlquilino`, `rut`, `direccion`, `telefono`, `fechaInicio`, `fechaFin`, `nombreObra`, `responsableObra`, `estatusNota`) VALUES
-(3, 'Nelson Rubio', '3809898T', 'Los altos', '04246142358', '2021-03-01', '2021-03-25', 'Urbanizacion', 'Francisco', 1),
-(4, 'Prueba', '2234566', 'Los altos', '434343434', '2021-04-02', '2021-04-30', 'Urbanizacion', 'Francisco', 1),
-(5, 'Prueba', '2234566', 'Los altos', '434343434', '2021-04-02', '2021-04-30', 'Urbanizacion', 'Francisco', 1);
+INSERT INTO `cabeceranota` (`idcabeceranota`, `nombreAlquilino`, `rut`, `direccion`, `telefono`, `fechaInicio`, `fechaFin`, `nombreObra`, `responsableObra`, `estatusNota`, `email`, `idChofer`) VALUES
+(3, 'Nelson Rubio', '3809898T', 'Los altos', '04246142358', '2021-03-01', '2021-03-25', 'Urbanizacion', 'Francisco', 1, '', 0),
+(4, 'Prueba', '2234566', 'Los altos', '434343434', '2021-04-02', '2021-04-30', 'Urbanizacion', 'Francisco', 1, '', 0),
+(5, 'Prueba', '2234566', 'Los altos', '434343434', '2021-04-02', '2021-04-30', 'Urbanizacion', 'Francisco', 1, '', 0),
+(6, 'prueba e email', '2343jhr', 'chile', '45345435', '2021-05-07', '2021-05-31', 'prueba de obra', 'nelson rubio', 1, 'prueba@gmail.com', 0),
+(7, 'errer', 'ererer', 'dfsdfsdfsdf', '43543543543', '2021-05-07', '2021-05-31', 'gsdgfdgfdg', 'gfgffgdgfdfgd', 1, 'nelsonrubio20@gmail.com', 39);
 
 -- --------------------------------------------------------
 
@@ -131,7 +134,9 @@ INSERT INTO `detallenota` (`iddetallenota`, `idcabeceranota`, `modeloarticulo`, 
 (2, 4, '2', 'dia', 4, 1),
 (3, 4, '3', 'mes', 8, 1),
 (4, 4, '4', 'dia', 2, 1),
-(5, 5, '4', 'dia', 4, 1);
+(5, 5, '4', 'dia', 4, 1),
+(6, 6, '30', 'dia', 4, 1),
+(7, 7, '30', 'dia', 5, 1);
 
 -- --------------------------------------------------------
 
@@ -175,6 +180,33 @@ INSERT INTO `estatusobra` (`idEstatus`, `estatus`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `repuesto`
+--
+
+CREATE TABLE `repuesto` (
+  `idBodega` int(11) NOT NULL,
+  `idRepuesto` int(11) NOT NULL,
+  `codOrigen` varchar(500) NOT NULL,
+  `CodInterno` varchar(500) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `proveedor` varchar(500) NOT NULL,
+  `nroFactura` varchar(500) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `precio` float NOT NULL,
+  `precioVenta` float NOT NULL,
+  `nombreRepuesto` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `repuesto`
+--
+
+INSERT INTO `repuesto` (`idBodega`, `idRepuesto`, `codOrigen`, `CodInterno`, `cantidad`, `proveedor`, `nroFactura`, `stock`, `precio`, `precioVenta`, `nombreRepuesto`) VALUES
+(5, 2, '258GLO', '1234568M', 50, 'stanly', '1245785', 10, 500, 1000, 'Martillo');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `roles`
 --
 
@@ -188,7 +220,8 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`idRol`, `nombreRol`) VALUES
-(1, 'Supervisor');
+(1, 'Supervisor'),
+(4, 'Chofer');
 
 -- --------------------------------------------------------
 
@@ -231,8 +264,9 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`idUsuario`, `nombreUsuario`, `clave`, `tipoUsuario`, `email`, `rut`, `idBodega`) VALUES
 (32, ' nelsonrubio20', '12345678', 1, 'nelsonrubio20@gmail.com', '', 0),
-(36, 'Francisco', '123456789', 3, 'francisco@gmail.com', '', 4),
-(37, 'Francia', '123456789', 1, 'francia@gmail.com', '', 0);
+(36, 'Francisco', '123456789', 4, 'francisco@gmail.com', '', 4),
+(37, 'Francia', '123456789', 1, 'francia@gmail.com', '', 0),
+(39, 'Isaac', '12345678', 4, 'isaac@gmail.com', '', 0);
 
 --
 -- Índices para tablas volcadas
@@ -275,6 +309,12 @@ ALTER TABLE `estatusobra`
   ADD PRIMARY KEY (`idEstatus`);
 
 --
+-- Indices de la tabla `repuesto`
+--
+ALTER TABLE `repuesto`
+  ADD PRIMARY KEY (`idRepuesto`);
+
+--
 -- Indices de la tabla `roles`
 --
 ALTER TABLE `roles`
@@ -300,25 +340,25 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `articulos`
 --
 ALTER TABLE `articulos`
-  MODIFY `idArticulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idArticulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de la tabla `bodegas`
 --
 ALTER TABLE `bodegas`
-  MODIFY `idBodega` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idBodega` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `cabeceranota`
 --
 ALTER TABLE `cabeceranota`
-  MODIFY `idcabeceranota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idcabeceranota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `detallenota`
 --
 ALTER TABLE `detallenota`
-  MODIFY `iddetallenota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `iddetallenota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `estatus`
@@ -333,10 +373,16 @@ ALTER TABLE `estatusobra`
   MODIFY `idEstatus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `repuesto`
+--
+ALTER TABLE `repuesto`
+  MODIFY `idRepuesto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `idRol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idRol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `tipobodega`
@@ -348,7 +394,7 @@ ALTER TABLE `tipobodega`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
