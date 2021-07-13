5403f38ca5856e8d1a8f7c5195a917c91f57122d
Nelson Rubio <nrubio@cobeca.com>
2021-07-13 14:33:39 -0400

    Base de datos actual

--- conexion/servirent.sql
+++ conexion/servirent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-07-2021 a las 19:48:03
+-- Tiempo de generación: 13-07-2021 a las 20:33:11
 -- Versión del servidor: 10.4.16-MariaDB
 -- Versión de PHP: 7.4.12
 
